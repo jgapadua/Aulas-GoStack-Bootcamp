591c754b11f5bc2caff9836871d93194f5cc73b1
jgapadua <joaogabrielpadua@gmail.com>
2022-01-31 14:55:09 -0300

    Listando projetos da API

--- nivel1/mobile/src/index.js
+++ nivel1/mobile/src/index.js
@@ -1,33 +1,53 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
 
-//não possuem valor semântico e não possuem estilização própria
-//Todos os componentes possuem por padrão "display: flex"
+import api from './services/api';
 
-//View ⇒ div, footer, header, main, aside, section
-//Text ⇒ p, span, strong, h1, h2, h3, ...
-export default function App() {
-  return (
+// Não possuem valor semântico (significado)
+// Não possuem estilização própria
+// Todos os componentes possuem por padrão "display: flex"
+
+// View ⇒ div, footer, header, main, aside, section
+// Text ⇒ p, span, strong, h1, h2, h3
+// ScrollView => View com scroll,  quando não estamos  expecificamente lidando com listas .
+//FlatList => Componente performático para listas dentro do React Native.
+// Não tem diferença visual entre a ScrollView e  FlatList, tiver uma lista muito grande a FlatList vai performar muito melhor, só mostra em tela o que tá visivel, tudo que não esta visivel em tela não renderiza por baixo dos panos.
+//SafeAreaView => Aréa segura da aplicação para ser exibido algum tipo de contéudo. Para que os dados não fiquem "vazando" abaixo da barra de status (principalmente no iOS), adicionamos o SafeAreaView.
+
+export default function  App(){
+  const [projects, setProjects] = useState([ ]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+  
+  return  (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Olá mundo</Text>
-      </View>
+    <StatusBar  barStyle='light-content' backgroundColor= '#7159c1'   />
+
+    <SafeAreaView style={styles.container}>
+    <FlatList
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item: project }) => (
+          <Text style={styles.project}>{project.title}</Text>
+        )}
+    />
+    </SafeAreaView>
     </>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#7159c1',
   },
 
-  title: {
-    color: "#fff",
-    fontSize: 32,
-    fontWeight: "bold",
+  project:{
+    color: '#FFF',
+    fontSize: 30,
   },
 });
---
+++ nivel1/mobile/src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+export default api;
+
+/* 
+- iOS com Emulador ⇒ localhost
+- iOS com dispositivo físico ⇒ IP da máquina
+- Android com Emulador ⇒ [localhost](http://localhost) (adb reverse)
+- Android com Emulador ⇒ 10.0.2.2 (Android Studio)
+- Android com Emulador ⇒ 10.0.3.2 (Genymotion)
+- Android com dispositivo físico ⇒ IP da máquina */
