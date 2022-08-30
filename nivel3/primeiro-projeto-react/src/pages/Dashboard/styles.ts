ed0289b61983d6c1e1ee0a805e49014d694981f2
jgapadua <joaogabrielpadua@gmail.com>
2022-08-30 20:10:43 -0300

    utilizando styled components

---
+++ nivel3/primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  /*   position: absolute;
+  width: 433px;
+  height: 112px;
+  left: 241px;
+  top: 172px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700; */
+  font-size: 48px;
+  /*   line-height: 56px; */
+
+  color: #3a3a3a;
+`;
