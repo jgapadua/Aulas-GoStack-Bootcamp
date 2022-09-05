fdd0cd2cf1a9ff9d7957e6d53cf19fe9d36209fc
jgapadua <joaogabrielpadua@gmail.com>
2022-09-05 12:34:13 -0300

    estilizando dashboard

--- nivel3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53950692?v=4"
+            alt="João Gabriel Pádua"
+          />
+          <div>
+            <strong>jgapadua/Aulas-GoStack-Bootcamp</strong>
+            <p>
+              Repositório contendo todos os códigos feitos nas aulas do Bootcamp
+              GoStack
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53950692?v=4"
+            alt="João Gabriel Pádua"
+          />
+          <div>
+            <strong>jgapadua/Aulas-GoStack-Bootcamp</strong>
+            <p>
+              Repositório contendo todos os códigos feitos nas aulas do Bootcamp
+              GoStack
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53950692?v=4"
+            alt="João Gabriel Pádua"
+          />
+          <div>
+            <strong>jgapadua/Aulas-GoStack-Bootcamp</strong>
+            <p>
+              Repositório contendo todos os códigos feitos nas aulas do Bootcamp
+              GoStack
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
