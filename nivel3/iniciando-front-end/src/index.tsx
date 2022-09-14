11ded54945ac0572a89640fe6d5ef51fc3e754df
jgapadua <joaogabrielpadua@gmail.com>
2022-09-14 16:13:46 -0300

    eslint

--- nivel3/iniciando-front-end/src/index.tsx
+++ nivel3/iniciando-front-end/src/index.tsx
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-
-  </React.StrictMode>
+  <React.StrictMode />,
 );
-
