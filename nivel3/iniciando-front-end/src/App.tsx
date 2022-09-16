dc8354e34efa2a91170ba8686c9775b5f13ead9f
jgapadua <joaogabrielpadua@gmail.com>
2022-09-16 15:11:53 -0300

    login de contexto

--- nivel3/iniciando-front-end/src/App.tsx
+++ nivel3/iniciando-front-end/src/App.tsx
@@ -4,17 +4,15 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <AuthContext.Provider value={{ name: 'João' }}>
-        <SignIn />
-      </AuthContext.Provider>
-      <GlobalStyle />
-    </>
-  );
-};
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
