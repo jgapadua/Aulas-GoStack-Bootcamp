be813ec05709e3bb8836c3bd49133fef0147d158
jgapadua <joaogabrielpadua@gmail.com>
2022-09-20 18:20:14 -0300

    Liskov Substitution Principle

---
+++ nivel4/iniciando-back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,8 @@
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IAppointmentsRepository {
+  create(data: Date): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
+}
+
+export default IAppointmentsRepository;
