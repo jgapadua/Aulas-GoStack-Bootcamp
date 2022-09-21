b1821d21ad932ecaaec33899a0a177cc90cab053
jgapadua <joaogabrielpadua@gmail.com>
2022-09-21 17:30:47 -0300

    usando controllers

---
+++ nivel4/iniciando-back-end/src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+class UserAvatarController {
+  public async update(req: Request, res: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+  }
+  /* appointmentsRouter.get('/', async (req, res) => {
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+}); */
+}
+
+export default UserAvatarController;
