95dbb4b333def0d5a639fa401704798929237641
jgapadua <joaogabrielpadua@gmail.com>
2022-09-23 12:48:09 -0300

    configurei Jest

---
+++ nivel4/iniciando-back-end/dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '8e900260a12e61aed76f19f83e56545a',
+        expiresIn: '1d',
+    },
+};
--- nivel4/iniciando-back-end/src/shared/infra/typeorm/migrations/1660707278122-AlterProviderFieldToProviderId.ts
+++ nivel4/iniciando-back-end/src/shared/infra/typeorm/migrations/1660707278122-AlterProviderFieldToProviderId.ts
@@ -6,7 +6,8 @@ import {
 } from 'typeorm';
 
 export default class AlterProviderFieldToProviderId1660707278122
-  implements MigrationInterface {
+  implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('appointments', 'provider');
 
