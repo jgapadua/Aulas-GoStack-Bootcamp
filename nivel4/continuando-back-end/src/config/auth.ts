0e4ab3e8220acd1a1a68f30dc245c753b0e382ba
jgapadua <joaogabrielpadua@gmail.com>
2022-10-14 14:17:24 -0300

    continuando-back-end do app

---
+++ nivel4/continuando-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '8e900260a12e61aed76f19f83e56545a',
+    expiresIn: '1d',
+  },
+};
---
+++ nivel4/continuando-back-end/src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+class UsersController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = container.resolve(CreateUserService);
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+  }
+  /* appointmentsRouter.get('/', async (req, res) => {
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+}); */
+}
+
+export default UsersController;
