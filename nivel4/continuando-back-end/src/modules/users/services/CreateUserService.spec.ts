294573723fc645dfc3a1371a82bcc66d0ee9be9a
jgapadua <joaogabrielpadua@gmail.com>
2022-10-14 17:44:53 -0300

    recuperacao de senha

--- nivel4/continuando-back-end/src/modules/users/services/CreateUserService.spec.ts
+++ nivel4/continuando-back-end/src/modules/users/services/CreateUserService.spec.ts
@@ -38,7 +38,7 @@ describe('CreateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       createUser.execute({
         name: 'Ciclano',
         email: 'fulano@example.com',
