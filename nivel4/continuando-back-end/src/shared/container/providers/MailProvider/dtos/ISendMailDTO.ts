9e3ecae9143a22f1e0014585a5275037d89fc049
jgapadua <joaogabrielpadua@gmail.com>
2022-10-16 05:29:39 -0300

    Template de emails

---
+++ nivel4/continuando-back-end/src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
@@ -0,0 +1,15 @@
+import IParseMailTemplateDTO from '../../MailTemplateProvide/dtos/IParseMailTemplateDTO';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ISendMailDTO {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplateDTO;
+}
+
+export default ISendMailDTO;
