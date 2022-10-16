cfb6cd3f8f95c2d43f842ecc67ab25f35ee92e0d
jgapadua <joaogabrielpadua@gmail.com>
2022-10-16 00:21:11 -0300

    Emails em desenvolvimento

--- nivel4/continuando-back-end/src/modules/users/services/UpdateUserAvatarService.ts
+++ nivel4/continuando-back-end/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,11 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
-import IUsersRepository from '../repositories/IUsersRepository';
 
-import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   user_id: string;
@@ -25,14 +26,16 @@ class UpdateUserAvatarService {
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
-      throw new AppError('Only authenticated users can change avatar', 401);
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
       await this.storageProvider.deleteFile(user.avatar);
     }
-    const filename = await this.storageProvider.saveFile(avatarFilename);
-    user.avatar = filename;
+
+    const fileName = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = fileName;
 
     await this.usersRepository.save(user);
 
